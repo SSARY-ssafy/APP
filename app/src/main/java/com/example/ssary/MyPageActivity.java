920a918c5bfaf16a008c8a97dc409f11acb9427b
Jeongseok Yoon <jeongseok.yoon@outlook.com>
2024-10-24 16:07:58 +0900

    241024_마이페이지

---
+++ app/src/main/java/com/example/ssary/MyPageActivity.java
@@ -0,0 +1,12 @@
+package com.example.ssary;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
